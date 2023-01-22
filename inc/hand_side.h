81c1298eddef93be3bc40f11e5f69f1e2c908bf0
Roy Ratcliffe <roy@ratcliffe.me>
2023-01-22 10:38:44 +0000

    docs: even more

--- inc/hand_side.h
+++ inc/hand_side.h
@@ -34,6 +34,7 @@ struct hand_side {
  * \param top Pointer to top
  * \param left Pointer to lefthand side
  * \param right Pointer to righthand side
+ * \return New left-right pair pushed to top or `NULL` if out of memory
  *
  * The push operation both pushes and creates if necessary. The top address is a
  * null pointer initially and this qualifies as a valid but empty hand-side
@@ -55,14 +56,67 @@ struct hand_side *push_hand_side(struct hand_side **top, void *left,
  */
 void free_hand_side(struct hand_side **top);
 
+/*!
+ * \brief Walks left-right chain
+ * \param top Pointer to top of chain
+ * \param and Pointer to function that visits left and right until it returns a
+ * non-null result
+ * \param arg Extra function argument
+ * \return Result of calling the function if non-null, else end of chain
+ *
+ * This generalised iteration function walks the left-right pairs one by one
+ * calling a function with three arguments: the left, the right and some
+ * context-related argument. The latter proves useful for older or deeply
+ * embedded compilers that cannot or should not capture its context using nested
+ * functions.
+ *
+ * The iteration stops when the function answers a non-null result. The
+ * iteration itself returns null when no iterated function call gives a non-null
+ * result.
+ */
 void *for_left_and_right_hand_side(struct hand_side **top,
                                    void *(*and)(void *left, void *right,
                                                 void *arg),
                                    void *arg);
 
+/*!
+ * \brief Right for lefthand side
+ * \param top Pointer to top of chain
+ * \param left Pointer to lefthand side
+ *
+ * Walks the chain looking for a matching left-hand side. Matching refers to
+ * comparison by reference only. It compares the pointers. Answers the
+ * right-hand side for the first match.
+ *
+ * Note that the search always stops at the first match, though technically
+ * there could be more than one match if the chain receives more than one push
+ * with the same left-hand side.
+ */
 void *right_for_left_hand_side(struct hand_side **top, void *left);
 
+/*!
+ * \brief Right for lefthand side by case-insensitive comparison
+ * \param top Pointer to top of chain
+ * \param left Pointer to null-terminated lefthand side string
+ *
+ * Right for left also supports insensitive string comparison where matching
+ * utilises `stricmp` for comparing the left-hand side with the given string.
+ * This naturally assumes that the left-hand sides represent pointers to
+ * null-terminated strings. The implementation throws away the `const`
+ * correctness by casting to the very generalised non-constant void pointer
+ * type.
+ */
 void *right_for_left_stricmp_hand_side(struct hand_side **top,
                                        const char *left);
 
+/*!
+ * \brief Left for righthand side
+ * \param top Pointer to top of chain
+ * \param right Pointer to righthand side
+ *
+ * Lookups typically work from left to right, but not always. When pairing
+ * strings (that is, pointers to null-terminated constant characters) with
+ * arbitrary pointers, for example, a reverse translation could prove handly by
+ * answering the string for some given pointer.
+ */
 void *left_for_right_hand_side(struct hand_side **top, void *right);
