3906efc55f121e8ac2b2baf7738fda489520f23c
royratcliffe <royratcliffe@users.noreply.github.com>
2023-02-03 20:46:18 +0000

    deploy: d08024dfc642d71aaa182565256c7c345aee80fa

--- search/all_3.js
+++ search/all_3.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['pop_0',['pop',['../structhand__side.html#ad29bf8a9a5db28bfd2b9a9ab530727b2',1,'hand_side']]],
-  ['push_5fhand_5fside_1',['push_hand_side',['../hand__side_8h.html#a63aed5ce0f44872c6aca400b341f2604',1,'push_hand_side(struct hand_side **top, void *left, void *right):&#160;hand_side.c'],['../hand__side_8c.html#a63aed5ce0f44872c6aca400b341f2604',1,'push_hand_side(struct hand_side **top, void *left, void *right):&#160;hand_side.c']]]
+  ['pop_5fleft_5fhand_5fside_1',['pop_left_hand_side',['../hand__side_8h.html#a5c8997eb3579bcfd76edf7ac48a21585',1,'pop_left_hand_side(struct hand_side **top, void *left):&#160;hand_side.c'],['../hand__side_8c.html#a5c8997eb3579bcfd76edf7ac48a21585',1,'pop_left_hand_side(struct hand_side **top, void *left):&#160;hand_side.c']]],
+  ['push_5fhand_5fside_2',['push_hand_side',['../hand__side_8h.html#a63aed5ce0f44872c6aca400b341f2604',1,'push_hand_side(struct hand_side **top, void *left, void *right):&#160;hand_side.c'],['../hand__side_8c.html#a63aed5ce0f44872c6aca400b341f2604',1,'push_hand_side(struct hand_side **top, void *left, void *right):&#160;hand_side.c']]]
 ];
--- search/functions_2.js
+++ search/functions_2.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['push_5fhand_5fside_0',['push_hand_side',['../hand__side_8h.html#a63aed5ce0f44872c6aca400b341f2604',1,'push_hand_side(struct hand_side **top, void *left, void *right):&#160;hand_side.c'],['../hand__side_8c.html#a63aed5ce0f44872c6aca400b341f2604',1,'push_hand_side(struct hand_side **top, void *left, void *right):&#160;hand_side.c']]]
+  ['pop_5fleft_5fhand_5fside_0',['pop_left_hand_side',['../hand__side_8h.html#a5c8997eb3579bcfd76edf7ac48a21585',1,'pop_left_hand_side(struct hand_side **top, void *left):&#160;hand_side.c'],['../hand__side_8c.html#a5c8997eb3579bcfd76edf7ac48a21585',1,'pop_left_hand_side(struct hand_side **top, void *left):&#160;hand_side.c']]],
+  ['push_5fhand_5fside_1',['push_hand_side',['../hand__side_8h.html#a63aed5ce0f44872c6aca400b341f2604',1,'push_hand_side(struct hand_side **top, void *left, void *right):&#160;hand_side.c'],['../hand__side_8c.html#a63aed5ce0f44872c6aca400b341f2604',1,'push_hand_side(struct hand_side **top, void *left, void *right):&#160;hand_side.c']]]
 ];
